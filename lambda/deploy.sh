e654f6ab84343fdc89759e7b32bc40d7cfab5364
Matthew Lesko-Krleza <mlesko1996@gmail.com>
2022-10-23 18:20:38 -0400

    Add package libs for deployment

--- lambda/deploy.sh
+++ lambda/deploy.sh
@@ -1,6 +1,8 @@
 mkdir lib
 mkdir dist
 
+pip3 freeze > requirements.txt
+pip3 install -r requirements.txt --target ./lib
 zip -r dist/deployment.zip ./lib/*
 zip -g dist/deployment.zip service.py
 aws lambda update-function-code --function-name MovieRecommendation --zip-file fileb://dist/deployment.zip
