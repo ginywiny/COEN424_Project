549a70029c0c084ed08fbc573b2dff9c8b7bc051
michael <michaelles97@gmail.com>
2022-12-06 00:27:15 -0500

    Add test case

--- send_test.py
+++ send_test.py
@@ -16,6 +16,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]=""
 #     "movieCount": 5
 # }
 
+# Test 1
 movie_map = {
     "movieName": {
       1: 5,
@@ -24,6 +25,15 @@ movie_map = {
     },
     "movieCount": 5
 }
+
+# Test 2: Star wars issue resolved
+# movie_map = {
+#     "movieName": {
+#       "50": 5,
+#       "719": 3
+#     },
+#     "movieCount": 5
+# }
 
 json_movie_map = json.dumps(movie_map)
 response = requests.post("https://hxudmk07fj.execute-api.us-east-1.amazonaws.com/default/MovieRecommendation", json=json_movie_map)
